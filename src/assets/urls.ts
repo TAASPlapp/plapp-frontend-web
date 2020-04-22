8386ad8ed30f35a9c5507ef997c7bbc75b0eee2a
st148705 <valentino.dicianni@edu.unito.it>
2020-04-22 21:34:43 +0200

    deployment - fixed

--- src/assets/urls.ts
+++ src/assets/urls.ts
@@ -1,6 +1,6 @@
 export const urls  = {
-  serverUrl: 'http://localhost:4001/',
-  apiServerUrl: 'http://localhost:4001/api/',
+  serverUrl: 'https://plapp-api-gataway.herokuapp.com',
+  apiServerUrl: 'https://plapp-api-gataway.herokuapp.com/api/',
   plantsInfoApiUrl: "https://plant-info-api.herokuapp.com/"
 
 };
